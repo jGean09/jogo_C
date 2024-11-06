f81afc4516f054929da2ac19b32930518a982220
JGean09 <josegeantlc@gmail.com>
2024-11-06 16:17:02 -0300

    feat; Criando função exibirTabela.

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void exibirTabela(char tab[3][3], int tam);
 
 int main(void) {
-    char tabela[3][3] = { {'0', '1', '2'}, {'3', '4', '5'}, {'6', '7', '8'} };
+    char tabela[3][3] = { {'r',' ',' '}, {' ', 'x', ' '}, {' ', ' ', 't'} };
+    exibirTabela(tabela, 3); 
+    return 0;
+}
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%c\t", tabela[i][j]);
+void exibirTabela(char tab[3][3], int tam) {
+    for (int i = 0; i < tam; i++) {
+        for (int j = 0; j < tam; j++) {
+            printf("%c\t", tab[i][j]);
         }
         printf("\n");
     }
-    
-    return 0;
 }
